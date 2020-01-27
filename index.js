85e143853851dd405c97799ca32b4c76c1e5ded3
Travis Franklin <travis.william.franklin@gmail.com>
2020-01-26 22:12:17 -0500

    Starting point for portfolio work.

---
+++ index.js
@@ -0,0 +1,20 @@
+// const http = require('http');
+// const express = require('express');
+// const app = express();
+// const server = http.createServer(app);
+// const es6Renderer = require('express-es6-template-engine');
+// app.engine('html', es6Renderer);
+// app.set('views', 'templates');
+// app.set('view engine', 'html');
+
+
+// const PORT = 3000;
+
+// app.get('/', (req, res) => {
+//     res.render('index');
+// })
+
+
+// server.listen(PORT, () => {
+//     console.log(`Listening on ${PORT}`);
+// });
