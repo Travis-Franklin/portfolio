b39e94efa8a7d72574d2f1339bbe804e9f6a3759
Travis Franklin <travis.william.franklin@gmail.com>
2020-02-04 16:14:04 -0500

    adds styling for nav bar

---
+++ scripts/index.js
@@ -0,0 +1,36 @@
+ 
+
+const sticky = new Sticky('#sticky-nav');
+
+
+
+
+
+
+
+
+
+
+
+
+// let stickyNav = document.querySelector('#sticky-nav');
+
+
+
+
+
+
+
+// console.log(stickyNav);
+
+// window.addEventListener('scroll', () => {
+//     const scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+//     console.log(scrollPosition);
+//     if (scrollPosition > 1148) {
+//         stickyNav.style.position = 'absolute';
+//         stickyNav.style.top = scrollPosition + 'px';
+//     } else {
+//         delete stickyNav.style.position;
+//         delete stickyNav.style.top;
+//     }
+// });
